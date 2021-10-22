1f5a007ade7be759bb1b387b98e8f93c9f38271f
Ivan Pozdnyakov <90461638+enotuuuk@users.noreply.github.com>
2021-10-22 11:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
  {
     price = document.getElementById('product_price').value;
     count = document.getElementById('product_count').value;
+    if(price>0 && count>0){
     sum = parseInt(price) * parseInt(count);
     document.getElementById('sum').innerHTML="Итоговая стоимость: "+ sum + " "+ "ru";
+    }
     return false;
 }
